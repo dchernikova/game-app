715bc7fe6c6e8a08acb931f89c4c0838ada50bc4
Daria Chernikova <darinka26223@mail.ru>
2020-06-19 18:04:13 +0300

    Updates

---
+++ precache-manifest.cc2cd4fe1b17f6e14cc907e593c11226.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8b962674c92a8d1bb07c5b7d3c3b5d",
+    "url": "/game-app/index.html"
+  },
+  {
+    "revision": "66a408f1fa4771987775",
+    "url": "/game-app/static/css/2.2f75dd9a.chunk.css"
+  },
+  {
+    "revision": "66a408f1fa4771987775",
+    "url": "/game-app/static/js/2.e66213f9.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/game-app/static/js/2.e66213f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b7b77497ed05fd96a8",
+    "url": "/game-app/static/js/main.d1398159.chunk.js"
+  },
+  {
+    "revision": "c1a912ea8f61e0c83979",
+    "url": "/game-app/static/js/runtime-main.34b88d3f.js"
+  },
+  {
+    "revision": "e66d089536a673646be8eb141f9ae7ce",
+    "url": "/game-app/static/media/footer-bg.e66d0895.jpg"
+  },
+  {
+    "revision": "0e90b6e8dee5b3656d58d08b90313aa6",
+    "url": "/game-app/static/media/game-1.0e90b6e8.jpg"
+  },
+  {
+    "revision": "201eb78c801fa8639125ebe20ac7e406",
+    "url": "/game-app/static/media/game-1.201eb78c.jpg"
+  },
+  {
+    "revision": "2193c029cd4e6d214168da4c62df68ec",
+    "url": "/game-app/static/media/game-1.2193c029.jpg"
+  },
+  {
+    "revision": "261a1c4157fa9da349f85a019a85a0c1",
+    "url": "/game-app/static/media/game-1.261a1c41.jpg"
+  },
+  {
+    "revision": "2bf59867ba35b2a9aec0aeb60c0f0540",
+    "url": "/game-app/static/media/game-1.2bf59867.jpg"
+  },
+  {
+    "revision": "ac059706a2991a9ed0154568fd121f87",
+    "url": "/game-app/static/media/game-1.ac059706.jpg"
+  },
+  {
+    "revision": "bcebf5e962eb63151853927c8aa5c186",
+    "url": "/game-app/static/media/game-1.bcebf5e9.jpg"
+  },
+  {
+    "revision": "bea9918f9e5e2806c04c114cd652863c",
+    "url": "/game-app/static/media/game-1.bea9918f.jpg"
+  },
+  {
+    "revision": "dae40a2a4f7016a89fbb889eaaf4465c",
+    "url": "/game-app/static/media/game-1.dae40a2a.jpg"
+  },
+  {
+    "revision": "fe88127e0150e49b82e778a3ff007b28",
+    "url": "/game-app/static/media/game-1.fe88127e.jpg"
+  },
+  {
+    "revision": "41294321346fe99df5d4470cc6119e8b",
+    "url": "/game-app/static/media/game-2.41294321.jpg"
+  },
+  {
+    "revision": "74ab34b3cf4136d94eee2033beb7dc9b",
+    "url": "/game-app/static/media/game-2.74ab34b3.jpg"
+  },
+  {
+    "revision": "866fb3a379456261c62ce08e5c3d722a",
+    "url": "/game-app/static/media/game-2.866fb3a3.jpg"
+  },
+  {
+    "revision": "937f9095fef2a40c55392ec00c1090ce",
+    "url": "/game-app/static/media/game-2.937f9095.jpg"
+  },
+  {
+    "revision": "9416e2a28f33daf24cb914fdb851e723",
+    "url": "/game-app/static/media/game-2.9416e2a2.jpg"
+  },
+  {
+    "revision": "beb9ce4128d73e250d45ef5a5a925684",
+    "url": "/game-app/static/media/game-2.beb9ce41.jpg"
+  },
+  {
+    "revision": "c50d1e40ea11586f9a4b175dc6d4675d",
+    "url": "/game-app/static/media/game-2.c50d1e40.jpg"
+  },
+  {
+    "revision": "d4762b8f0bb61836806db636112da7f2",
+    "url": "/game-app/static/media/game-2.d4762b8f.jpg"
+  },
+  {
+    "revision": "e4377bdc0838ba3804757969848d65e5",
+    "url": "/game-app/static/media/game-2.e4377bdc.jpg"
+  },
+  {
+    "revision": "e5f608121fd86e8793a28ab63c3b33fc",
+    "url": "/game-app/static/media/game-2.e5f60812.jpg"
+  },
+  {
+    "revision": "0ffe3628f1796726c603b8333ca9129c",
+    "url": "/game-app/static/media/game-3.0ffe3628.jpg"
+  },
+  {
+    "revision": "31fae946818abb8e9be3854af85f53d9",
+    "url": "/game-app/static/media/game-3.31fae946.jpg"
+  },
+  {
+    "revision": "4f9b704b70287408ccfabfcbfc8f7fd3",
+    "url": "/game-app/static/media/game-3.4f9b704b.jpg"
+  },
+  {
+    "revision": "9562c6cf3a03081bd71e8c533d741cc6",
+    "url": "/game-app/static/media/game-3.9562c6cf.jpg"
+  },
+  {
+    "revision": "a4e7d611e9943fa957549693b8dd52a4",
+    "url": "/game-app/static/media/game-3.a4e7d611.jpg"
+  },
+  {
+    "revision": "bd9d38e586faab1e4481aa495f5908d8",
+    "url": "/game-app/static/media/game-3.bd9d38e5.jpg"
+  },
+  {
+    "revision": "c7ed4f155c99b04826509f9bd76daab5",
+    "url": "/game-app/static/media/game-3.c7ed4f15.jpg"
+  },
+  {
+    "revision": "d7266e9ae063b8b0685d211c6689dee7",
+    "url": "/game-app/static/media/game-3.d7266e9a.jpg"
+  },
+  {
+    "revision": "d8b02d3cdd99aeab96568d34fc63ecff",
+    "url": "/game-app/static/media/game-3.d8b02d3c.jpg"
+  },
+  {
+    "revision": "edcc5d86ce523623eb0a586153911fd3",
+    "url": "/game-app/static/media/game-3.edcc5d86.jpg"
+  },
+  {
+    "revision": "f04ac2de303ca376d0d59a340d071541",
+    "url": "/game-app/static/media/game-4.f04ac2de.jpg"
+  },
+  {
+    "revision": "1bfc474e716648a4c1b6f0ccba7d9ccc",
+    "url": "/game-app/static/media/main.1bfc474e.jpg"
+  },
+  {
+    "revision": "57a427a12fb537ffacb4429d08630d90",
+    "url": "/game-app/static/media/main.57a427a1.jpg"
+  },
+  {
+    "revision": "613065ae4b36de177eddc7a6ddcb7284",
+    "url": "/game-app/static/media/main.613065ae.jpg"
+  },
+  {
+    "revision": "6bc1259c82cc4b0977d1372654615f8c",
+    "url": "/game-app/static/media/main.6bc1259c.jpg"
+  },
+  {
+    "revision": "738da4c13c1f4ef3e27b9b0018f9d17f",
+    "url": "/game-app/static/media/main.738da4c1.jpg"
+  },
+  {
+    "revision": "744f3e98ff8505798e795c1ba6c28695",
+    "url": "/game-app/static/media/main.744f3e98.jpg"
+  },
+  {
+    "revision": "b1682ea345d445cf02cc6d91922fcbba",
+    "url": "/game-app/static/media/main.b1682ea3.jpg"
+  },
+  {
+    "revision": "b27c2f02f341783a82b7ab1a816ffcfd",
+    "url": "/game-app/static/media/main.b27c2f02.jpg"
+  },
+  {
+    "revision": "b6c2a608713af1d4fbd0268795af9ff1",
+    "url": "/game-app/static/media/main.b6c2a608.jpg"
+  },
+  {
+    "revision": "efd231ac724c729edf318cf48f16b021",
+    "url": "/game-app/static/media/main.efd231ac.jpg"
+  },
+  {
+    "revision": "198732a3dc0eee93b658b8e74d4028af",
+    "url": "/game-app/static/media/preview.198732a3.jpg"
+  },
+  {
+    "revision": "1ebb40342f372a431b8456ba56a6700c",
+    "url": "/game-app/static/media/preview.1ebb4034.jpg"
+  },
+  {
+    "revision": "39da39468e57ac06dfb63d0e847d2e4b",
+    "url": "/game-app/static/media/preview.39da3946.jpg"
+  },
+  {
+    "revision": "73dc9590ba67f67a23b4a74813f2b1fb",
+    "url": "/game-app/static/media/preview.73dc9590.jpg"
+  },
+  {
+    "revision": "809067455077b8cff087672839619248",
+    "url": "/game-app/static/media/preview.80906745.jpg"
+  },
+  {
+    "revision": "a70f1e9659823e743701f201e2ea6595",
+    "url": "/game-app/static/media/preview.a70f1e96.jpg"
+  },
+  {
+    "revision": "ae5a661666a64a180496f89a7cfccf06",
+    "url": "/game-app/static/media/preview.ae5a6616.jpg"
+  },
+  {
+    "revision": "c8247e61245057367502675dc2530fe8",
+    "url": "/game-app/static/media/preview.c8247e61.jpg"
+  },
+  {
+    "revision": "f7f6c71971f09dfab774f9d98365af82",
+    "url": "/game-app/static/media/preview.f7f6c719.jpg"
+  },
+  {
+    "revision": "fab1f74e8a4159865b0e42821f91f449",
+    "url": "/game-app/static/media/preview.fab1f74e.jpg"
+  }
+]);
